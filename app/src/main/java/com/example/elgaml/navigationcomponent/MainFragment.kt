12e1f095fcf62f95fa36d3e493e51536fdb23e09
mahmoudelgamal1997 <mahmoudelgamal1997@gmail.com>
2020-07-22 18:05:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/elgaml/navigationcomponent/MainFragment.kt
@@ -0,0 +1,51 @@
+package com.example.elgaml.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(),View.OnClickListener {
+
+    lateinit var navController:NavController
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController= Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0!!.id){
+            R.id.view_balance_btn-> navController.navigate(R.id.action_mainFragment_to_viewBalance)
+
+
+            R.id.send_money_btn-> {
+                navController.navigate(R.id.action_mainFragment_to_chooseFragment)
+            }
+            R.id.view_transactions_btn-> {
+                navController.navigate(R.id.action_mainFragment_to_viewTransactions)
+            }
+        }
+
+
+    }
+}
